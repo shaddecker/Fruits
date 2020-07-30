b634dd361ea82a18c24c714b56a4004294b031f8
shaddecker <shaddecker5@gmail.com>
2020-07-30 15:01:13 -0500

    fixing errors after jwt

--- fruits_app/server.js
+++ fruits_app/server.js
@@ -8,6 +8,19 @@ const app = express();
 const jwt = require("jsonwebtoken");
 const cookieParser = require("cookie-parser");
 
+const verifyToken = (req, res, next) => {
+  let token = req.cookies.jwt; // COOKIE PARSER GIVES YOU A .cookies PROP, WE NAMED OUR TOKEN jwt
+  console.log("Cookies: ", req.cookies.jwt);
+  jwt.verify(token, process.env.JWT_SECRET, (err, decodedUser) => {
+    if (err || !decodedUser) {
+      return res.status(401).json({ error: "Unauthorized Request" });
+    }
+    req.user = decodedUser; // ADDS A .user PROP TO REQ FOR TOKEN USER
+    console.log(decodedUser);
+    next();
+  });
+};
+
 app.use(cookieParser());
 app.use(express.static("public"));
 
@@ -22,9 +35,9 @@ app.get("/", (req, res) => {
   res.render("users/index.ejs");
 });
 
-app.use("/fruits", require("./controllers/fruitsController.js"));
-app.use("/users", require("./controllers/usersController.js"));
 app.use("/auth", require("./controllers/authController.js"));
+app.use("/fruits", require("./controllers/fruitsController.js"));
+app.use("/users", verifyToken,require("./controllers/usersController.js"));
 
 //listen for express requests on port 3000
 app.listen(process.env.PORT, () => {
